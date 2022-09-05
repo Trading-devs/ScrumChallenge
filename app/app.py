3458f4004ffe61e4b70b1c2dfa2aca2aad20af64
Diego Alejandro Cortes Roa <cortesroadiegoalejandro@gmail.com>
2022-09-05 11:21:44 -0500

    frontend: template version 3

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from pymongo import MongoClient
 
@@ -6,13 +7,34 @@ app = Flask(__name__)
 # conexion = MongoDB(app)
 client = MongoClient('localhost')
 database = client['currencyapp']
+timer = ''
+
+
+def updateDatabase():
+    # conexion = client['conexion']
+    # print(conexion)
+    # database.update_one({'_id': conexion._id}, {'$set': {'amount': conexion.amount}})
+    print('Updating database...')
+
+
+def countdown(countdownTime):
+    while countdownTime:
+        mins, secs = divmod(countdownTime, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        countdownTime -= 1
+
 
 def getDocuments(currency='dava'):
     collection = database[currency]
     return collection.find({})
 
+
 @app.route('/<currency>')
 def index(currency='dava'):
+
+    currencyName = ''
 
     if currency == 'dava':
         currencyName = 'Endava'
@@ -33,25 +55,29 @@ def index(currency='dava'):
         currencyName = 'Cardano'
 
     data = {
-        'currency': currencyName,
+        'currency': "💱 " + currencyName,
         'abbreviation': currency.upper(),
         'documents': getDocuments(currency),
     }
 
     return render_template('index.html', data=data)
 
+
 @app.before_request
 def before_request():
     print('Antes de la petición...')
+
 
 @app.after_request
 def after_request(response):
     print('Después de la petición')
     return response
 
-def pagina_no_encontrada(error):
-    return redirect(url_for('/dava'))
+
+def pageNotFound(error):
+    return redirect(url_for('index/dava'))
+
 
 if __name__ == '__main__':
-    app.register_error_handler(404, pagina_no_encontrada)
+    app.register_error_handler(404, pageNotFound)
     app.run(debug=True, port=5000)
