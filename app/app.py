12de9dae8e4bc4e43a37e3a05937db2df07e0ddf
Diego Alejandro Cortes Roa <cortesroadiegoalejandro@gmail.com>
2022-09-04 20:08:35 -0500

    frontend: template version 2

--- app/app.py
+++ app/app.py
@@ -1,83 +1,57 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify
-# from flask_mysqldb import MySQL
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Conexión MySQL
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = '123456'
-# app.config['MYSQL_DB'] = 'base_datos'
+# conexion = MongoDB(app)
+client = MongoClient('localhost')
+database = client['currencyapp']
 
-# conexion = MySQL(app)
+def getDocuments(currency='dava'):
+    collection = database[currency]
+    return collection.find({})
 
+@app.route('/<currency>')
+def index(currency='dava'):
+
+    if currency == 'dava':
+        currencyName = 'Endava'
+
+    elif currency == 'ec':
+        currencyName = 'Ecopetrol'
+
+    elif currency == 'ibm':
+        currencyName = 'IBM'
+
+    elif currency == 'btc':
+        currencyName = 'Bitcoin'
+
+    elif currency == 'eth':
+        currencyName = 'Ethereum'
+
+    elif currency == 'ada':
+        currencyName = 'Cardano'
+
+    data = {
+        'currency': currencyName,
+        'abbreviation': currency.upper(),
+        'documents': getDocuments(currency),
+    }
+
+    return render_template('index.html', data=data)
 
 @app.before_request
 def before_request():
-    print("Antes de la petición...")
-
+    print('Antes de la petición...')
 
 @app.after_request
 def after_request(response):
-    print("Después de la petición")
+    print('Después de la petición')
     return response
 
-
-# @app.route('/')
-# def index():
-#     # return "<h1>UskoKruM2010 - Suscríbete!</h1>"
-#     cursos = ['PHP', 'Python', 'Java', 'Kotlin', 'Dart', 'JavaScript']
-#     data = {
-#         'titulo': 'Index123',
-#         'bienvenida': '¡Saludos!',
-#         'cursos': cursos,
-#         'numero_cursos': len(cursos)
-#     }
-#     return render_template('index.html', data=data)
-
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-
-@app.route('/contacto/<nombre>/<int:edad>')
-def contacto(nombre, edad):
-    data = {
-        'titulo': 'Contacto',
-        'nombre': nombre,
-        'edad': edad
-    }
-    return render_template('contacto.html', data=data)
-
-
-def query_string():
-    print(request)
-    print(request.args)
-    print(request.args.get('param1'))
-    print(request.args.get('param2'))
-    return "Ok"
-
-
-# @app.route('/cursos')
-# def listar_cursos():
-#    data = {}
-#    try:
-#        cursor = conexion.connection.cursor()
-#        sql = "SELECT codigo, nombre, creditos FROM curso ORDER BY nombre ASC"
-#        cursor.execute(sql)
-#        cursos = cursor.fetchall()
-#        # print(cursos)
-#        data['cursos'] = cursos
-#        data['mensaje'] = 'Exito'
-#    except Exception as ex:
-#        data['mensaje'] = 'Error...'
-#    return jsonify(data)
-
 def pagina_no_encontrada(error):
-    # return render_template('404.html'), 404
-    return redirect(url_for('index'))
+    return redirect(url_for('/dava'))
 
 if __name__ == '__main__':
-    app.add_url_rule('/query_string', view_func=query_string)
     app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True, port=5000)
--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -1,1 +1,15 @@
-// alert('Hola Mundo!!!');
+const coinsContainer = document.querySelector(".coins");
+const containerTable = document.querySelector(".container-table");
+let currency;
+
+coinsContainer.addEventListener("click", function (e) {
+  e.preventDefault();
+  const target = e.target.closest(".coin");
+  if (!target) return;
+  updateUrl(target);
+});
+
+const updateUrl = function (newCurrency) {
+  const path = newCurrency.querySelector(".coin__link").getAttribute("href");
+  location.href = `/${path}`;
+};
