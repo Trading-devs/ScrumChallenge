f7195f961793c814dd994652ff9eff4313a57b57
CristianAcostaDuarte <101611537+CristianAcostaDuarte@users.noreply.github.com>
2022-09-07 22:13:14 -0500

    Backend finished!!

--- app/ex.py
+++ app/ex.py
@@ -1,39 +1,134 @@
-
-import pymongo
+import threading
+from flask import Flask, render_template, url_for, redirect
+from flask import Flask, request, jsonify, Response
+from pymongo import MongoClient
+from flask_pymongo import PyMongo
 import json 
 import requests
+import time
 import pandas as pd
-from flask import Flask
-from flask import Flask, request, jsonify, Response
-from flask_pymongo import PyMongo
 from bson import json_util
 from bson.objectid import ObjectId
-import time
-
 from datetime import datetime, date
 
+# Conection = MongoDB(app)
+client = MongoClient('localhost')
+database = client['challengedb']
 
-#Hora, minuto y segundo
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+#A collection for each database
+dava=database["dava"]
+ec=database["ec"]
+ibm=database["ibm"]
+ada=database["ada"]
+btc=database["btc"]
+eth=database["eth"]
 
-#Dia
-today = date.today()
+def get_dava():
+    records  = dava.find()
+    records =list(records)
+    df=pd.DataFrame(records)
+    return df
 
-#Fecha completa
-current_date=(str(today) + ':' +str(current_time))
+def get_ec():
+    records  = ec.find()
+    records =list(records)
+    df=pd.DataFrame(records)
+    return df
 
-#Insertar fecha
-Endava=requests.get('https://financialmodelingprep.com/api/v3/quote-short/DAVA?apikey=2c18aef9c283317435d76672c51df35d')
-Endava=Endava.json()
-Endava[0]['date'] =current_date
+def get_ibm():
+    records = ibm.find()
+    records =list(records)
+    df=pd.DataFrame(records)
+    return df
+
+def get_btc():
+    records  = btc.find()
+    records =list(records)
+    df=pd.DataFrame(records)
+    return df
+
+def get_eth():
+    records  = eth.find()
+    records =list(records)
+    df=pd.DataFrame(records)
+    return df
+
+def get_ada():
+    records  = ada.find()
+    records =list(records)
+    df=pd.DataFrame(records)
+    return df
 
 
+def stock_values():
+    dava=get_dava()
+    current_dava = dava['price'].values[0]
+    mean_dava = round(dava['price'].mean(axis=0),2)
+    percent_dava = ((current_dava-mean_dava)/mean_dava)*100
+    
+    if(percent_dava > 20 or percent_dava < -20):
+        print('sentdava')
+    
+    ec=get_ec()
+    current_ec = ec['price'].values[0]
+    mean_ec = round(ec['price'].mean(axis=0),2)
+    percent_ec = ((current_ec-mean_ec)/mean_ec)*100
+    
+    if(percent_ec > 20 or percent_ec< -20):
+        print('sentec')
+    
+    ibm=get_ibm()
+    current_ibm = ibm['price'].values[0]
+    mean_ibm = round(ibm['price'].mean(axis=0),2)
+    percent_ibm = ((current_ibm-mean_ibm)/mean_ibm)*100
+    
+    if(percent_ibm > 20 or percent_ibm < -20):
+        print('sentibm')
+    
+    btc=get_btc()
+    current_btc = btc['price'].values[0]
+    mean_btc = round(btc['price'].mean(axis=0),2)
+    percent_btc = ((current_btc-mean_btc)/mean_btc)*100
+    
+    if(percent_btc > 20 or percent_btc < -20):
+        print('sentbtc')
+    
+    eth=get_eth()
+    current_eth = eth['price'].values[0]
+    mean_eth = round(eth['price'].mean(axis=0),2)
+    percent_eth = ((current_eth-mean_eth)/mean_eth)*100
+    
+    if(percent_eth > 20 or percent_eth< -20):
+        print('senteth')
+    
+    ada=get_ada()
+    current_ada = ada['price'].values[0]
+    mean_ada = round(ada['price'].mean(axis=0),2)
+    
+    percent_ada = ((current_ada-mean_ada)/mean_ada)*100
+    
+    if(percent_ada > 20 or percent_ada < -20):
+        print('sentada')
 
+def Average(lst):
+    return sum(lst) / len(lst)
 
+dava=get_dava()
+current_dava = dava['price'].values[-1]
+mean_dava =round(Average(dava['price'].values[:-1]),2)
+percent_dava = ((current_dava-mean_dava)/mean_dava)*100
 
-#Insertar fecha
-Endava[0]['date'] =current_date
+x_dava=list(dava['date'].values)
+y_dava=list(dava['price'].values)
+x_ec=list(ec['date'].values)
+y_ec=list(ec['price'].values)
+x_ibm=list(ibm['date'].values)
+y_ibm=list(ibm['price'].values)
+x_btc=list(btc['date'].values)
+y_btc=list(btc['price'].values)
+x_eth=list(eth['date'].values)
+y_eth=list(eth['price'].values)
+x_ada=list(ada['date'].values)
+y_ada=list(ada['price'].values)
 
-print(Endava)
-
+print(x_dava)
--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -1,5 +1,6 @@
 const coinsContainer = document.querySelector(".coins");
 const containerTable = document.querySelector(".container-table");
+const countdownTimer = document.querySelector(".timer");
 let currency;
 
 coinsContainer.addEventListener("click", function (e) {
@@ -13,3 +14,31 @@ const updateUrl = function (newCurrency) {
   const path = newCurrency.querySelector(".coin__link").getAttribute("href");
   location.href = `/${path}`;
 };
+
+const countDownHandler = function (timeLeft) {
+  // Set time to 5 minutes
+  let time = timeLeft;
+  countdownTimer.textContent = "";
+
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const second = String(Math.trunc(time % 60)).padStart(2, 0);
+    // In each call, print the remaining time to UI
+    countdownTimer.textContent = `${min}:${second}`;
+    // Decrese 1s
+    time--;
+
+    // When 0 seconds, refresh the timer
+    if (time < 0) {
+      document.location.reload(true);
+      time = 300;
+    }
+  };
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
+
+countDownHandler(timeLeft);
